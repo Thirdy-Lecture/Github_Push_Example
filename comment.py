60f7684fdae7d0f4a171a6a8bbeca1180a2ab66e
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2024-08-24 12:41:06 +0800

    Update comment.py

--- comment.py
+++ comment.py
@@ -1,4 +1,4 @@
 # Python comment
 # This is a comment
 
-print ("Hi, I am Shamel Jane D. Francisco BSIT 2-A") #This is my name, year and section
+print ("Hi, I am Shamel Jane D. Francisco BSIT 2-A") #This is my name, year and section SSS
